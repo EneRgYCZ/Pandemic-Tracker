adc1a24f253f17b88f2b6d4887e04514b9511067
User <user@Users-MacBook-Pro.local>
2020-12-05 03:06:47 +0200

    Raport Medical

--- V_Map/screens/HomeScreen.js
+++ V_Map/screens/HomeScreen.js
@@ -1,31 +1,241 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet, StatusBar } from 'react-native';
+import { 
+  View, 
+  Text, 
+  Button, 
+  StyleSheet, 
+  StatusBar, 
+  Alert, 
+  ScrollView, 
+  Switch, 
+  TouchableOpacity } from 'react-native';
 import { useTheme } from '@react-navigation/native';
+import Constants from 'expo-constants';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+import Accordion from 'react-native-collapsible/Accordion';
 
-const HomeScreen = ({navigation}) => {
+const BACON_IPSUM =
+  'Text Text Text';
 
-  const { colors } = useTheme();
+const CONTENT = [
+  {
+    title: 'Durere de cap',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Nas infundat',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Durere in gat',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Febra',
+    content: BACON_IPSUM,
+  },
+  {
+    title: 'Durere abdominala',
+    content: BACON_IPSUM,
+  },
+];
 
-  const theme = useTheme();
-  
+const SELECTORS = [
+  {
+    title: 'Durere de cap',
+    value: 0,
+  },
+  {
+    title: "Nas infundat",
+    value: 1,
+  },
+  {
+    title: 'Durere in gat',
+    value: 2,
+  },
+];
+
+const SELECTORS2 = [
+  {
+    title: 'Febra',
+    value: 3,
+  },
+  {
+    title: "Durere abdomnala",
+    value: 4,
+  },
+  {
+    title: 'Durere in gat',
+    value: 5,
+  },
+];
+
+export default class HomeScreen extends React.Component {
+  state = {
+    activeSections: [],
+    collapsed: true,
+    multipleSelect: false,
+  };
+
+  toggleExpanded = () => {
+    this.setState({ collapsed: !this.state.collapsed });
+  };
+
+  setSections = sections => {
+    this.setState({
+      activeSections: sections.includes(undefined) ? [] : sections,
+    });
+  };
+
+  renderHeader = (section, _, isActive) => {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.header, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Text style={styles.headerText}>{section.title}</Text>
+      </Animatable.View>
+    );
+  };
+
+  renderContent(section, _, isActive) {
+    return (
+      <Animatable.View
+        duration={400}
+        style={[styles.content, isActive ? styles.active : styles.inactive]}
+        transition="backgroundColor"
+      >
+        <Animatable.Text animation={isActive ? 'bounceIn' : undefined}>
+          {section.content}
+        </Animatable.Text>
+      </Animatable.View>
+    );
+  }
+
+  render() {
+    const { multipleSelect, activeSections } = this.state;
     return (
       <View style={styles.container}>
-        <StatusBar barStyle= { theme.dark ? "light-content" : "dark-content" }/>
-        <Text style={{color: colors.text}}>Home Screen</Text>
-      <Button
-        title="Go to details screen"
-        onPress={() => navigation.navigate("Details")}
-      />
+        <ScrollView contentContainerStyle={{ paddingTop: 30 }}>
+          <Text style={styles.title}>Raport Medical</Text>
+          <View style={styles.selectors}>
+            <Text style={styles.selectTitle}>Simptome:</Text>
+            {SELECTORS.map(selector => (
+              <TouchableOpacity
+                key={selector.title}
+                onPress={() => this.setSections([selector.value])}
+              >
+                <View style={styles.selector}>
+                  <Text
+                    style={
+                      activeSections.includes(selector.value) &&
+                      styles.activeSelector
+                    }
+                  >
+                    {selector.title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <View style={styles.selectors}>
+            {SELECTORS2.map(selector => (
+              <TouchableOpacity
+                key={selector.title}
+                onPress={() => this.setSections([selector.value])}
+              >
+                <View style={styles.selector}>
+                  <Text
+                    style={
+                      activeSections.includes(selector.value) &&
+                      styles.activeSelector
+                    }
+                  >
+                    {selector.title}
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            ))}
+          </View>
+          <Collapsible collapsed={this.state.collapsed} align="center">
+            <View style={styles.content}>
+              
+            </View>
+          </Collapsible>
+          <Accordion
+            activeSections={activeSections}
+            sections={CONTENT}
+            touchableComponent={TouchableOpacity}
+            expandMultiple={multipleSelect}
+            renderHeader={this.renderHeader}
+            renderContent={this.renderContent}
+            duration={300}
+            onChange={this.setSections}
+          />
+        </ScrollView>
       </View>
     );
-};
-
-export default HomeScreen;
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    alignItems: 'center', 
-    justifyContent: 'center'
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    paddingTop: Constants.statusBarHeight,
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 22,
+    fontWeight: '300',
+    marginBottom: 20,
+  },
+  header: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  headerText: {
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  content: {
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  active: {
+    backgroundColor: 'rgba(255,255,255,1)',
+  },
+  inactive: {
+    backgroundColor: 'rgba(245,252,255,1)',
+  },
+  selectors: {
+    marginBottom: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  selector: {
+    backgroundColor: '#F5FCFF',
+    padding: 10,
+  },
+  activeSelector: {
+    fontWeight: 'bold',
+  },
+  selectTitle: {
+    fontSize: 14,
+    fontWeight: '500',
+    padding: 10,
+  },
+  multipleToggle: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+    alignItems: 'center',
+  },
+  multipleToggle__title: {
+    fontSize: 16,
+    marginRight: 8,
   },
 });
